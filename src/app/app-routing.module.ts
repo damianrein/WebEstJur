b641b235fd083870cd4c49c11a68b9274b4392e1
damianrein <damianrein464@gmail.com>
2024-10-28 23:20:46 -0300

    -

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent}
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'**', redirectTo:'', pathMatch:'full'}
 ];
 
 @NgModule({
