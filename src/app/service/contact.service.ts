3af9e39081faf423eba341785c48f4d3c1ca818d
damianrein <damianrein464@gmail.com>
2024-10-31 23:27:33 -0300

    -

--- src/app/service/contact.service.ts
+++ src/app/service/contact.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,5 @@ import { Injectable } from '@angular/core';
 export class ContactService {
 
   constructor() { }
+
 }
